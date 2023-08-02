b77d0948b35ea7ff3d915cdc898078bf9c530e1c
Zachary Stence <zachstence@gmail.com>
2023-08-01 19:16:19 -0500

    feat: layout with navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,15 @@
   import '../app.css';
 </script>
 
-<slot />
+<div class="w-screen h-screen overflow-hidden flex flex-col">
+  <header class="shrink-0">
+    <nav class="navbar bg-base-300">
+      <!-- TODO better name -->
+      <a class="btn btn-ghost text-xl mx-auto" href="/">Crossword Generator</a>
+    </nav>
+  </header>
+
+  <main class="flex-1 overflow-x-hidden overflow-y-auto">
+    <slot />
+  </main>
+</div>
