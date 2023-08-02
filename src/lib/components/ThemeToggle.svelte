906308d245eb27a22c01cf33939a0be40048a8fd
Zachary Stence <zachstence@gmail.com>
2023-08-01 20:13:21 -0500

    feat: theme toggle

---
+++ src/lib/components/ThemeToggle.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import { onMount } from 'svelte';
+  import { themeChange } from 'theme-change';
+
+  onMount(() => {
+    themeChange(false);
+  });
+</script>
+
+<div class={$$props.class}>
+  <button
+    class="btn btn-ghost swap swap-rotate"
+    data-toggle-theme="dark,light"
+    data-act-class="swap-active"
+  >
+    <div class="swap-on">Dark</div>
+    <div class="swap-off">Light</div>
+  </button>
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,18 @@
 <script lang="ts">
+  import { ThemeToggle } from '$lib/components';
+
   import '../app.css';
 </script>
 
 <div class="w-screen h-screen overflow-hidden flex flex-col">
   <header class="shrink-0">
     <nav class="navbar bg-base-300">
+      <div class="navbar-start" />
+
       <!-- TODO better name -->
-      <a class="btn btn-ghost text-xl mx-auto" href="/">Crossword Generator</a>
+      <a class="navbar-center btn btn-ghost text-xl mx-auto" href="/">Crossword Generator</a>
+
+      <ThemeToggle class="navbar-end" />
     </nav>
   </header>
 
